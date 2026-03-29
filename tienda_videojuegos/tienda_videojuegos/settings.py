79ba2389bd948471b4a65d3b94e7861578ae1acf
SebastianSST <auxiliarsst@fundacionudea.org.co>
2026-03-28 23:06:36 -0500

    Se agrega tema b/w
    Se añade boton con persistencia para el tema de la pagina, se agrega el static para las paginas.

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -56,8 +56,8 @@ ROOT_URLCONF = 'tienda_videojuegos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'], # Directorio de templates global
+        'APP_DIRS': True, # Busca los templates en los apps
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
