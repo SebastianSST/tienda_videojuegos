67bea67e0b0962307e3ed8b69c53a7c71c19b664
SebastianSST <auxiliarsst@fundacionudea.org.co>
2026-03-29 20:07:35 -0500

    Se agrega app carrito

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo',
     'home',
-    'buscador'
+    'catalogo',
+    'buscador',
+    'usuarios',
+    'carrito',
 ]
 
 MIDDLEWARE = [
